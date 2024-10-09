8016216df6fc16c74a1f64874a96b7d92fd66bbe
ADITYARAJ PATIL <119296947+aspatil0@users.noreply.github.com>
2024-10-09 19:57:21 +0530

    day1 js

---
+++ script.js
@@ -0,0 +1,37 @@
+// to select a element means the tag of html
+
+// let x= document.querySelectorAll("h1");
+// console.log(x);
+
+
+// -------------------------------------------
+
+//change colour with the help of javascript by using htmlk tags
+
+// let x= document.querySelector("h1");
+// x.style.color="red"
+// x.style.backgroundColor="yellow";
+// x.innerHTML="adi";
+
+
+// ----------------------------------------------
+
+
+//chaage text in some intervals means 2-3 seconds
+
+// let x= document.querySelector("h1");
+// setTimeout(function()
+// {
+//     x.innerHTML="adi";
+//     x.style.backgroundColor="red";
+// },5000)
+
+// ------------------------------------------------
+
+// EVENT LISTENS IT IS MAIN IN JS
+
+
+// let x = document.querySelector("#adi");
+let x = document.getElementById("adi");
+x.style.backgroundColor="red";
+
