c7efd8e23f105792f97d698edf6bb8d94144ed27
ADITYARAJ PATIL <119296947+aspatil0@users.noreply.github.com>
2025-05-02 20:31:41 +0530

    Props.jsx

---
+++ Props.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+const Cat = (props) => {
+  return (
+    <div>
+      hii i am {props.name}
+    </div>
+
+  )
+ 
+}
+
+export default Cat
+
+
+// #------------------------------------------------#
+
+
+  import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>
+        <p>The total count is: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Click here</button>
+        <button
+          onClick={() => {
+            if (count > 0) {
+              setCount(count - 1);
+            }
+          }}
+          disabled={count === 0}
+        >
+          Minus
+        </button>
+        <button onClick={() => setCount(0)}>Zero</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
