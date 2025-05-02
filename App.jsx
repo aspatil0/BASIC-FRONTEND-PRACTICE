63f483dd5d794c5354c97fbb2cdfb94a56002b79
ADITYARAJ PATIL <119296947+aspatil0@users.noreply.github.com>
2025-05-02 20:32:32 +0530

    Create App.jsx
    this is same for the props concepts

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Cat from './components/Cat';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+   <Cat  name='adi'/>
+  );
+}
+
+export default App;
