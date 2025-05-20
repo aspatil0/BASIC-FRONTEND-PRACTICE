d7961640f6690ab46e89161816d39e04f3554120
ADITYARAJ PATIL <119296947+aspatil0@users.noreply.github.com>
2025-05-20 22:58:57 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -5,6 +5,7 @@ import Counter from './components/Counter';
 function App() {
   return (
    // <Cat  name='adi'/>  it is the props
+    // when we take many data in cat we can use props to dynamicaly chnage elements
   );
 }
 
