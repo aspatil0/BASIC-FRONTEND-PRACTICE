25e58deca86460654167976b977c412dd7475a72
ADITYARAJ PATIL <119296947+aspatil0@users.noreply.github.com>
2025-05-24 23:49:23 +0530

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,19 @@
+const potatoQuotes = [
+  "🥔 You're a hot potato!",
+  "🥔 Even mashed, you're amazing!",
+  "🥔 Stay calm and potato on.",
+  "🥔 Fries before guys!",
+  "🥔 You're baked to perfection!",
+  "🥔 Hash-tag you're awesome!",
+  "🥔 Don't be a couch potato... unless it's comfy.",
+  "🥔 Sweet or not, you're still a potato legend!"
+];
+
+function motivateWithPotatoes() {
+  setInterval(() => {
+    const randomQuote = potatoQuotes[Math.floor(Math.random() * potatoQuotes.length)];
+    console.log(randomQuote);
+  }, 2000);
+}
+
+motivateWithPotatoes();
